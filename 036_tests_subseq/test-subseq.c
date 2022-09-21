b5d6903bfcc7b65b32712301c9b3aa39e76edc63
yh342 <yh342@duke.edu>
2022-09-21 12:13:12 -0400

    Did assignment 36

--- 036_tests_subseq/test-subseq.c
+++ 036_tests_subseq/test-subseq.c
@@ -4,45 +4,38 @@
 size_t maxSeq(int * array, size_t n);
 
 int main() {
-  size_t expected_answer = 1;
   int arr1[] = {1};
-  if (maxSeq(arr1, 1) != expected_answer)
+  if (maxSeq(arr1, 1) != 1)
     return EXIT_FAILURE;
   int arr2[] = {1, 2, 4, 3};
-  expected_answer = 3;
-  if (maxSeq(arr2, 4) != expected_answer)
+  if (maxSeq(arr2, 4) != 3)
     return EXIT_FAILURE;
   int arr3[] = {1, 1, 1};
-  expected_answer = 1;
-  if (maxSeq(arr3, 3) != expected_answer)
+  if (maxSeq(arr3, 3) != 1)
     return EXIT_FAILURE;
   int arr4[] = {-1000000000, -1, 0, 1, 1000000000};
-  expected_answer = 5;
-  if (maxSeq(arr4, 5) != expected_answer)
+  if (maxSeq(arr4, 5) != 5)
     return EXIT_FAILURE;
-  int arr5[] = {-1, 1, 0, 1, 0, -1};
-  expected_answer = 2;
-  if (maxSeq(arr5, 6) != expected_answer)
+  int arr5[] = {-1, -1, 0, 1, 2, 3};
+  if (maxSeq(arr5, 6) != 5)
     return EXIT_FAILURE;
   int arr6[] = {1, 0};
-  expected_answer = 1;
-  if (maxSeq(arr6, 2) != expected_answer)
+  if (maxSeq(arr6, 0) != 0)
     return EXIT_FAILURE;
-  int arr7[] = {};
-  expected_answer = 0;
-  if (maxSeq(arr7, 0) != expected_answer)
+  int arr7[] = {1, 2, 3, 4, 4};
+  if (maxSeq(arr7, 5) != 4)
     return EXIT_FAILURE;
   int arr8[] = {-1, 0, 0, 1};
-  expected_answer = 2;
-  if (maxSeq(arr8, 4) != expected_answer)
+  if (maxSeq(arr8, 4) != 2)
     return EXIT_FAILURE;
-  int arr9[] = {1, 2};
-  expected_answer = 2;
-  if (maxSeq(arr9, 2) != expected_answer)
+  int arr9[] = {-1, -2, -3, -4};
+  if (maxSeq(arr9, 4) != 1)
     return EXIT_FAILURE;
-  int arr10[] = {3, -1, 10};
-  expected_answer = 1;
-  if (maxSeq(arr10, 3) != expected_answer)
+  int arr10[] = {15, 13, 11, 1};
+  if (maxSeq(arr10, 4) != 1)
+    return EXIT_FAILURE;
+  int arr11[] = {1, 3, 5, 7, 2, 4, 6};
+  if (maxSeq(arr11, 7) != 4)
     return EXIT_FAILURE;
 
   return EXIT_SUCCESS;
