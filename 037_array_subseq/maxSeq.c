8c5968d525a79344c4a092116dc9f3d3d2d9b25b
yh342 <yh342@duke.edu>
2022-09-24 19:58:06 -0400

    Did assignment 37

---
+++ 037_array_subseq/maxSeq.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+size_t maxSeq(int * array, size_t n) {
+  if (n == 0 || n == 1)
+    return n;
+  int max_num = 1;
+  int cur_num = 1;
+  for (size_t i = 1; i < n; i++) {
+    if (array[i] > array[i - 1]) {
+      cur_num++;
+    }
+    else {
+      if (cur_num > max_num) {
+        max_num = cur_num;
+        cur_num = 1;
+      }
+    }
+  }
+  return cur_num > max_num ? cur_num : max_num;
+}
