2da85ab7246a55af1bb07252301f34e616e3d348
yh342 <yh342@duke.edu>
2022-09-28 11:07:46 -0400

    Did assignment 41

--- 041_reverse_str/reverse.c
+++ 041_reverse_str/reverse.c
@@ -4,6 +4,16 @@
 
 void reverse(char * str) {
   //WRITE ME!
+  //SURE! I WILL WRITE ON YOU!
+  int size_str = 0;
+  while (str[size_str] != '\0')
+    size_str++;
+  //printf("size: %d\n", size_str);
+  for (int j = 0, k = size_str - 1; j < k; j++, k--) {
+    char tmp = str[j];
+    str[j] = str[k];
+    str[k] = tmp;
+  }
 }
 
 int main(void) {
@@ -13,7 +23,8 @@ int main(void) {
   char str3[] = "Captain's log, Stardate 42523.7";
   char str4[] = "Hello, my name is Inigo Montoya.";
   char str5[] = "You can be my wingman anyday!";
-  char str6[] = "Executor Selendis! Unleash the full power of your forces! There may be no tomorrow!";
+  char str6[] = "Executor Selendis! Unleash the full power of your forces! There may be "
+                "no tomorrow!";
   char * array[] = {str0, str1, str2, str3, str4, str5, str6};
   for (int i = 0; i < 7; i++) {
     reverse(array[i]);
