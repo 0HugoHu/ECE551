6ed2496e441fa9ff96e00bd17b1b80d2c857fc7b
yh342 <yh342@duke.edu>
2022-10-03 17:25:58 -0400

    Did assignment 53

--- 053_sort_lines/sortLines.c
+++ 053_sort_lines/sortLines.c
@@ -14,8 +14,61 @@ void sortData(char ** data, size_t count) {
   qsort(data, count, sizeof(char *), stringOrder);
 }
 
+void sort_stdin(void * f) {
+  char ** lines = NULL;
+  lines = malloc(sizeof(*lines));
+  char * cur_line = NULL;
+  cur_line = (char *)malloc(sizeof(char));
+  int num_line = 0;
+  while (getline(&cur_line, NULL, f) >= 0) {
+    lines = realloc(lines, (num_line + 1) * sizeof(*lines));
+    lines[num_line++] = cur_line;
+    cur_line = NULL;
+  }
+
+  sortData(lines, num_line);
+
+  for (int i = 0; i < num_line; i++) {
+    printf("%s", lines[i]);
+    free(lines[i]);
+  }
+
+  free(cur_line);
+  free(lines);
+}
+
+void sort_lines(int argc, char ** argv) {
+  for (int i = 0; i < argc - 1; i++) {
+    FILE * f = fopen(argv[i + 1], "r");
+    if (f == NULL) {
+      fprintf(stderr, "Cannot open some of the files!\n");
+      exit(EXIT_FAILURE);
+    }
+
+    sort_stdin(f);
+
+    if (fclose(f) != 0) {
+      fprintf(stderr, "Cannot close some of the files!\n");
+      exit(EXIT_FAILURE);
+    }
+  }
+}
+
 int main(int argc, char ** argv) {
   //WRITE YOUR CODE HERE!
+  if (argc == 1) {
+    char * line = NULL;
+    getline(&line, NULL, stdin);
+    sort_stdin(line);
+    free(line);
+  }
+  else if (argc > 1) {
+    sort_lines(argc, argv);
+  }
+  else {
+    fprintf(stderr, "Error argument number!");
+    exit(EXIT_FAILURE);
+  }
 
   return EXIT_SUCCESS;
 }
