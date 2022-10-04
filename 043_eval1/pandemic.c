266531b6bf74c18d2fe46fd93f5828bd86e21746
yh342 <yh342@duke.edu>
2022-10-04 18:30:45 -0400

    Test 10

--- 043_eval1/pandemic.c
+++ 043_eval1/pandemic.c
@@ -19,7 +19,7 @@ country_t parseLine(char * line) {
 
   //Read for country name
   int index = 0;
-  for (; line[index] != ',' && index < 256; index++) {
+  for (; line[index] != ',' && index < MAX_NAME_LEN; index++) {
     //Do not contains a comma
     if (line[index] == '\0') {
       fprintf(stderr, "Data must be in format: country_name,population!\n");
@@ -27,16 +27,24 @@ country_t parseLine(char * line) {
     }
     ans.name[index] = line[index];
   }
+  //Country name too long or 0 length
+  if (index == MAX_NAME_LEN - 1 || index == 0) {
+    fprintf(stderr, "Country name is too long or null!\n");
+    exit(EXIT_FAILURE);
+  }
   ans.name[index++] = '\0';
 
   //Read for population
   uint64_t population = 0;
   char c = 0;
+  //Remove spaces after comma
+  while (line[index++] == ' ');
+  //Get value
   while ((c = line[index++]) != '\0' && c != '\n') {
     if (c >= '0' && c <= '9') {
       population = population * 10 + c - '0';
       //Overflow
-      if (population > 1500000000) {
+      if (population > 1400000000) {
         fprintf(stderr, "Wrong population! Overflow!\n");
         exit(EXIT_FAILURE);
       }
@@ -70,14 +78,14 @@ void calcRunningAvg(unsigned * data, size_t n_days, double * avg) {
     fprintf(stderr, "data: No data is found!\n");
     exit(EXIT_FAILURE);
   }
+  //No enough days, return succeed
   if (n_days < 7) {
-    fprintf(stderr, "ndays: At least 7 data is required!");
-    exit(EXIT_FAILURE);
+    exit(EXIT_SUCCESS);
   }
 
   double num_avg = 0.0;
   size_t index = 0;
-  //Read first 6 data
+  //Read first 6 days data
   for (; index < 6; index++) {
     if (data[index] >= 0) {
       num_avg += data[index];
@@ -92,8 +100,10 @@ void calcRunningAvg(unsigned * data, size_t n_days, double * avg) {
   //Calculate rolling average
   for (; index < n_days; index++) {
     if (data[index] >= 0) {
+      //Rolling numbers, add the current day
       num_avg += data[index];
       avg[index - 6] = num_avg / 7.0;
+      //Delete the (current - 6) day data
       num_avg -= data[index - 6];
     }
     //Bad data
@@ -121,11 +131,13 @@ void calcCumulative(unsigned * data, size_t n_days, uint64_t pop, double * cum) 
   }
 
   //Calculate cumulative sum per 100,000
-  double num_cum = 0.0;
+  uint64_t num_cum = 0;
   for (int i = 0; i < n_days; i++) {
     if (data[i] >= 0) {
       num_cum += data[i];
-      cum[i] = num_cum * 100000 / pop;
+      //First times 100,0000 avoid 0 result
+      //64-bit num_cum ensures no overflow
+      cum[i] = num_cum * 100000.0 / pop;
     }
     //Bad data
     else {
@@ -158,7 +170,7 @@ void printCountryWithMax(country_t * countries,
     exit(EXIT_FAILURE);
   }
 
-  unsigned max = 0;
+  uint64_t max = 0;
   int country_index = 0;
   //Iterate through countries
   for (size_t i = 0; i < n_countries; i++) {
@@ -173,7 +185,7 @@ void printCountryWithMax(country_t * countries,
   }
 
   //Print result
-  printf("%s has the most daily cases with %u\n", countries[country_index].name, max);
+  printf("%s has the most daily cases with %lu\n", countries[country_index].name, max);
 
   //CODE END HERE
 }
