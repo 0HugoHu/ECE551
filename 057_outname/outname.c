1215abc3d711dad7949b9a88131d4196eb7855b3
yh342 <yh342@duke.edu>
2022-10-12 16:47:02 -0400

    Did assignment 57

--- 057_outname/outname.c
+++ 057_outname/outname.c
@@ -1,8 +1,15 @@
+#include "outname.h"
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "outname.h"
 
 char * computeOutputFileName(const char * inputName) {
   //WRITE ME
+  const char * suffix = ".count";
+  int length = strlen(inputName) + 7;
+  char * outputName = malloc(length * sizeof(*outputName));
+  strcpy(outputName, inputName);
+  strcat(outputName, suffix);
+  return outputName;
 }
